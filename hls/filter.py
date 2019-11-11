f73c7f3414fa67f501a0cfd45aa5e4601d666f08
joo <joo>
2019-11-11 21:55:05 +0900

    add basic filter

---
+++ hls/filter.py
@@ -0,0 +1,18 @@
+
+from hls.fileIO import *
+
+class filter():
+    def __init__(self, src, dst, w, h, dtype):
+        pass
+    
+    
+
+'''
+'''
+if __name__ == '__main__':
+    src = "/home/joo/work/eclipse/python/img/1.bin"
+    dst = "/home/joo/work/eclipse/python/img/2.bin"
+    
+    adder = filter(src, dst, 320, 240, "uint16")
+    
+    
