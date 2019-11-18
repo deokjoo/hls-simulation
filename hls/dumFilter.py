f424db48cd5136c7489639e8a5fb9bd5275c18b7
joo <joo>
2019-11-18 20:11:04 +0900

    add dummy

---
+++ hls/dumFilter.py
@@ -0,0 +1,36 @@
+'''
+'''
+from hls.filter import filter, filterWrapper
+import numpy as np
+
+
+'''
+samplef for filter
+'''
+class ip_dummmy(filterWrapper):
+    def __init__(self, src, w, h):
+        self.pading = 3
+        self.kernel = self.pading * 2 + 1            
+        self.filter = filter(src, src+".ahe", w, h, 0, self.kernel, "uint16")
+    
+    '''
+    opeation for pixel
+    '''
+    def op(self, patch):
+        return 0 
+    
+    '''
+    statitic for image
+    '''
+    def st(self, img  ):
+        pass
+
+'''
+'''
+if __name__ == '__main__':
+    src = "/home/joo/work/eclipse/python/img/1.bin"
+    
+    adder = ip_dummmy(src, 320, 240)
+    adder.do()    
+    
+    
